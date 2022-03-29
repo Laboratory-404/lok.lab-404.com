551e15dc80f5105e471d506f93e30516835aa251
DeKaDeNcE <admin@dekadence.ro>
2022-03-29 17:42:33 +0300

    More work

---
+++ docs/service-worker.js
@@ -0,0 +1,63 @@
+const build = [
+  "/_app/start-84199c9b.js",
+  "/_app/pages/__layout.svelte-ceff997b.js",
+  "/_app/pages/__error.svelte-d3f55883.js",
+  "/_app/pages/index.svelte-3d5ce773.js",
+  "/_app/assets/pages/index.svelte-4329fac1.css",
+  "/_app/chunks/vendor-40b1686d.js",
+  "/_app/assets/vendor-dc80082f.css"
+];
+const files = [
+  "/favicon.png",
+  "/images/logos/logo-lok-192.png",
+  "/images/logos/logo-lok-256.png",
+  "/images/logos/logo.png",
+  "/manifest.webmanifest",
+  "/robots.txt"
+];
+const version = "1648564890943";
+const ASSETS = `cache${version}`;
+const to_cache = build.concat(files);
+const staticAssets = new Set(to_cache);
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(ASSETS).then((cache) => cache.addAll(to_cache)).then(() => {
+    self.skipWaiting();
+  }));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then(async (keys) => {
+    for (const key of keys) {
+      if (key !== ASSETS)
+        await caches.delete(key);
+    }
+    await self.clients.claim();
+  }));
+});
+async function fetchAndCache(request) {
+  const cache = await caches.open(`offline${version}`);
+  try {
+    const response = await fetch(request);
+    await cache.put(request, response.clone());
+    return response;
+  } catch (err) {
+    const response = await cache.match(request);
+    if (response)
+      return response;
+    throw err;
+  }
+}
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || event.request.headers.has("range"))
+    return;
+  const url = new URL(event.request.url);
+  const isHttp = url.protocol.startsWith("http");
+  const isDevServerRequest = url.hostname === self.location.hostname && url.port !== self.location.port;
+  const isStaticAsset = url.host === self.location.host && staticAssets.has(url.pathname);
+  const skipBecauseUncached = event.request.cache === "only-if-cached" && !isStaticAsset;
+  if (isHttp && !isDevServerRequest && !skipBecauseUncached) {
+    event.respondWith((async () => {
+      const cachedAsset = isStaticAsset && await caches.match(event.request);
+      return cachedAsset || fetchAndCache(event.request);
+    })());
+  }
+});
