6305e0a59e109d3eaeed0bd00f0830f614fdccdf
ZaDarkSide <ZaDarkSide@users.noreply.github.com>
2022-03-31 04:29:36 +0300

    More work

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -4,8 +4,6 @@ import { persist, localStorage } from '$lib/persist'
 // noinspection JSUnusedGlobalSymbols
 export const db = persist(writable({
 	clear: false,
-	clear2: false,
-	clear3: false,
 	wallet: '0xB00Fc62B8B22243779A35effb8B98f18193daa09',
 	contract: '0x6CC462bc49ceCFE943Bc4F477b23b92906e6074F',
 	lands: ['134223', '134224', '134225', '133969', '133968', '133967', '133711', '133712', '133713', '133455', '133456', '133457'],
